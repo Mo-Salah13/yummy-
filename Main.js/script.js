816fcdcc75c69c7bfc839d8b87d6a6300d58b5bc
Mo-Salah13 <muhamedsalah1996@gmail.com>
2024-01-29 01:22:29 +0200

    clean ready function

--- Main.js/script.js
+++ Main.js/script.js
@@ -1,54 +1,32 @@
-$(document).ready(function () {
-    let sideBarWidth = $('nav').innerWidth();
-    $('#sideBar').css('left', -sideBarWidth);
-    $('#open i').click(function () {
-        if ($('#sideBar').css('left') == '0px') {
-            $('#sideBar').animate({ left: -sideBarWidth }, 500)
-            $('#open i').addClass('fa-angles-right').removeClass('fa-angles-left');
-        }
-        else {
-            $('#sideBar').animate({ left: '0px' }, 500)
-            $('#open i').addClass('fa-angles-left').removeClass('fa-angles-right');
-        }
-    })
-
-    let displayMeals = document.querySelector('#displayMeals');
-    let result = [];
-
-    async function getCategories() {
-        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
-        result = await response.json();
-        // console.log(result);
-        showCategories();
+let sideBarWidth = $('nav').innerWidth();
+$('#sideBar').css('left', -sideBarWidth);
+$('#open i').click(function () {
+    if ($('#sideBar').css('left') == '0px') {
+        $('#sideBar').animate({ left: -sideBarWidth }, 500)
+        $('#open i').addClass('fa-angles-right').removeClass('fa-angles-left');
     }
-
-    getCategories();
-    function showCategories() {
-        let container = ``
-        for (let i = 0; i < result.categories.length; i++) {
-            container += `
-        <div class="col-md-3 ">
-          <div class="meal">
-            <img class="image" src="${result.categories[i].strCategoryThumb}" alt="Meal Photo">
-            <a href="#" class="title">${result.categories[i].strCategory}</a>
-          </div>
-        </div>`
-        }
-        displayMeals.innerHTML = container
+    else {
+        $('#sideBar').animate({ left: '0px' }, 500)
+        $('#open i').addClass('fa-angles-left').removeClass('fa-angles-right');
     }
+})
 
+// let displayMeals = document.getElementById('displayMeals');
+let result = [];
 
-    async function homeMeal(){
-        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
-        result= await response.json();
-        // console.log(result);
-        showRandom()
-    }
+async function homeMeal() {
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
+    result = await response.json();
+    // console.log(result);
+    showHomeMeal()
+}
+
 homeMeal();
-function showRandom (){
-    let container =``
-    for(let i=0; i<result.meals.length; i++){
-        container +=`
+
+function showHomeMeal() {
+    let container = ``
+    for (let i = 0; i < result.meals.length; i++) {
+        container += `
         <div class="col-md-3 ">
         <div class="meal">
             <img class="image" src="${result.meals[i].strMealThumb}" alt="Meal Photo">
@@ -56,7 +34,31 @@ function showRandom (){
         </div>
     </div>`
     }
-    displayMeals.innerHTML=container;
+    document.getElementById('displayMeals').innerHTML = container;
+}
+let catResult = [];
+
+async function getCategories() {
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+    catResult = await response.json();
+    // console.log(result);
+    showCategories();
+}
+
+// getCategories();
+function showCategories() {
+    // window.alert('hello')
+    let container = ``
+    for (let i = 0; i < catResult.categories.length; i++) {
+        container += `
+    <div class="col-md-3 ">
+      <div class="meal">
+        <img class="image" src="${catResult.categories[i].strCategoryThumb}" alt="Meal Photo">
+        <a href="#" class="title">${catResult.categories[i].strCategory}</a>
+      </div>
+    </div>`
+    }
+    document.getElementById('displayMeals').innerHTML = container;
 }
 
 
@@ -89,7 +91,6 @@ function showRandom (){
 
 
 
-})
 
 
 
